ac516da96b971e0e67dd747a3ea2554ac098fd71
Jakub Vulgan <jvulgan@redhat.com>
2025-11-10 14:13:45 +0100

    add /healthz endpoint and consolidate /app endpoint

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import "net/http"
 
 func main() {
     mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(".")))
-    mux.Handle("/assets", http.FileServer(http.Dir("assets")))
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    mux.HandleFunc("/healthz", func (w http.ResponseWriter, req *http.Request){
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
     server := http.Server{
         Addr: ":8080",
         Handler: mux,
