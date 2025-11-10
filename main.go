3489cf10b6d6a9a8f7b4747ee707889b6bf63c27
Jakub Vulgan <jvulgan@redhat.com>
2025-11-10 22:58:17 +0100

    chore: format using go fmt

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) middlewareMetricsGet(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "text/html")
-    w.WriteHeader(http.StatusOK)
-    data := fmt.Sprintf(`<html>
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	data := fmt.Sprintf(`<html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
     <p>Chirpy has been visited %d times!</p>
