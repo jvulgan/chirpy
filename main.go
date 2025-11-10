cb3cdacca94564123cc3442ab307bd7d5f0545f1
Jakub Vulgan <jvulgan@redhat.com>
2025-11-10 13:25:49 +0100

    add serving of logo.png

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
     mux := http.NewServeMux()
     mux.Handle("/", http.FileServer(http.Dir(".")))
+    mux.Handle("/assets", http.FileServer(http.Dir("assets")))
     server := http.Server{
         Addr: ":8080",
         Handler: mux,
