0482b2bc44b2e2c9483090b36a992837978b49c7
Jakub Vulgan <jvulgan@redhat.com>
2025-11-10 15:46:05 +0100

    add middleware for keeping track of server hits

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsGet(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+}
 
 func main() {
-    mux := http.NewServeMux()
-    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-    mux.HandleFunc("/healthz", func (w http.ResponseWriter, req *http.Request){
-        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte("OK"))
-    })
-    server := http.Server{
-        Addr: ":8080",
-        Handler: mux,
-    }
-    server.ListenAndServe()
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", apiCfg.middlewareMetricsGet)
+	mux.HandleFunc("/reset", apiCfg.middlewareMetricsReset)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
