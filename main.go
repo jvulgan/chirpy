f1174e72147dbb72bd908dc9a58682b6610a4569
Jakub Vulgan <jvulgan@redhat.com>
2025-11-11 14:25:28 +0100

    add support for postgres

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	"github.com/jvulgan/chirpy/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -35,42 +43,50 @@ func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, req *http.Re
 }
 
 func validateChirp(w http.ResponseWriter, req *http.Request) {
-    type params struct {
-        Body string `json:"body"`
-    }
-    var p params
+	type params struct {
+		Body string `json:"body"`
+	}
+	var p params
 
-    decoder := json.NewDecoder(req.Body)
-    if err := decoder.Decode(&p); err != nil {
-        respondWithError(w, http.StatusInternalServerError, "Could not decode parameters", err)
-        return
-    }
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not decode parameters", err)
+		return
+	}
 
-    if len(p.Body) > 140 {
-        respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
-        return
-    }
+	if len(p.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
 
-    type response struct {
-        CleanedBody string `json:"cleaned_body"`
-    }
-    respBody := response{CleanedBody: replaceBadWords(p.Body)}
-    respondWithJSON(w, http.StatusOK, respBody)
+	type response struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	respBody := response{CleanedBody: replaceBadWords(p.Body)}
+	respondWithJSON(w, http.StatusOK, respBody)
 }
 
 func main() {
-	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("err connecting to postgres: %s", err)
+		return
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, dbQueries: *dbQueries}
 	mux := http.NewServeMux()
-    // app handler
+	// app handler
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-    // api calls handlers
+	// api calls handlers
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-    mux.HandleFunc("POST /api/validate_chirp", validateChirp)
-    // admin api handlers
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
+	// admin api handlers
 	mux.HandleFunc("GET /admin/metrics", apiCfg.middlewareMetricsGet)
 	mux.HandleFunc("POST /admin/reset", apiCfg.middlewareMetricsReset)
 	server := http.Server{
