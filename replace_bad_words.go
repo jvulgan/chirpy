2e8f5485ad9f2b188456a670d4ee2c9255ce805a
Jakub Vulgan <jvulgan@redhat.com>
2025-11-11 14:22:44 +0100

    chore: format using go fmt

--- replace_bad_words.go
+++ replace_bad_words.go
@@ -6,14 +6,14 @@ import (
 )
 
 func replaceBadWords(original string) string {
-    bad_words := []string{"kerfuffle", "sharbert", "fornax"}
-    var clean []string
-    for _, word := range strings.Split(original, " ") {
-        if slices.Contains(bad_words, strings.ToLower(word)) {
-            clean = append(clean, "****")
-        } else {
-            clean = append(clean, word)
-        }
-    }
-    return strings.Join(clean, " ")
+	bad_words := []string{"kerfuffle", "sharbert", "fornax"}
+	var clean []string
+	for _, word := range strings.Split(original, " ") {
+		if slices.Contains(bad_words, strings.ToLower(word)) {
+			clean = append(clean, "****")
+		} else {
+			clean = append(clean, word)
+		}
+	}
+	return strings.Join(clean, " ")
 }
--- response.go
+++ response.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "log"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
-    if err != nil {
-        log.Println(err)
-    }
-    if code > 499 {
-        log.Printf("Responding with 5XX code: %s", msg)
-    }
-    type errResponse struct {
-        Error string `json:"error"`
-    }
-    respondWithJSON(w, code, errResponse{Error: msg})
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX code: %s", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errResponse{Error: msg})
 }
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-    dat, err := json.Marshal(payload)
-    if err != nil {
-        log.Printf("Error while marshalling response %s", err)
-        w.WriteHeader(500)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(code)
-    w.Write(dat)
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error while marshalling response %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
 }
