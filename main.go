469fc765c1ebf1860c99cbe845ad4fae2645e38f
Jakub Vulgan <jvulgan@redhat.com>
2025-11-12 11:10:04 +0100

    add api for adding a user

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"net/http"
 	"os"
 	"sync/atomic"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/jvulgan/chirpy/internal/database"
 	_ "github.com/lib/pq"
@@ -17,6 +19,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	dbQueries      database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -40,6 +43,11 @@ func (cfg *apiConfig) middlewareMetricsGet(w http.ResponseWriter, req *http.Requ
 
 func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, req *http.Request) {
 	cfg.fileserverHits = atomic.Int32{}
+	if cfg.platform == "dev" {
+		if err := cfg.dbQueries.DeleteUsers(req.Context()); err != nil {
+			log.Printf("failed to call DeleteUsers: %s", err)
+		}
+	}
 }
 
 func validateChirp(w http.ResponseWriter, req *http.Request) {
@@ -66,16 +74,50 @@ func validateChirp(w http.ResponseWriter, req *http.Request) {
 	respondWithJSON(w, http.StatusOK, respBody)
 }
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) addUser(w http.ResponseWriter, req *http.Request) {
+	type params struct {
+		Email string `json:"email"`
+	}
+	var p params
+
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&p); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not decode parameters", err)
+		return
+	}
+
+	u, err := cfg.dbQueries.CreateUser(req.Context(), p.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create user", err)
+		return
+	}
+	user := User{
+		ID:        u.ID,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+		Email:     u.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Printf("err connecting to postgres: %s", err)
 		return
 	}
 	dbQueries := database.New(db)
-	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, dbQueries: *dbQueries}
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}, dbQueries: *dbQueries, platform: platform}
 	mux := http.NewServeMux()
 	// app handler
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
@@ -86,6 +128,7 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.addUser)
 	// admin api handlers
 	mux.HandleFunc("GET /admin/metrics", apiCfg.middlewareMetricsGet)
 	mux.HandleFunc("POST /admin/reset", apiCfg.middlewareMetricsReset)
